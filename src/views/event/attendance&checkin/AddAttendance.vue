00b4457ec40ff30c4d2e093b57ab3e4b0769d95c
Oladapo Daniel <oladapodaniel10@gmail.com>
2022-08-09 23:01:30 +0100

    disabled account name input

--- src/views/event/attendance&checkin/AddAttendance.vue
+++ src/views/event/attendance&checkin/AddAttendance.vue
@@ -602,6 +602,7 @@
                       placeholder="account name"
                       v-model="accountName"
                       ref="accNameRef"
+                      disabled
                     />
                   </div>
                   <div
