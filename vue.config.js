23687f628a532fd99e287c6b5f2783b777f693d3
Oladapo Daniel <oladapodaniel10@gmail.com>
2023-02-21 05:08:19 +0100

    removed ssr config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,26 @@
+// Commented code are configurations for Server side rendering
+
+// const { WebpackManifestPlugin } = require('webpack-manifest-plugin')
+// const nodeExternals = require('webpack-node-externals')
+
 module.exports = {
-    lintOnSave: false
+    lintOnSave: false,
+    // chainWebpack: (webpackConfig) => {
+    //     if (!process.env.SSR) return;
+    //     webpackConfig.target('node')
+    //     webpackConfig.output.libraryTarget('commonjs2')
+    //     webpackConfig.plugin('manifest').use(new WebpackManifestPlugin({ fileName: 'ssr-manifest.json' }));
+    //     webpackConfig.externals(nodeExternals({ allowlist: /\.(css|vue)$/ }))
+    //     webpackConfig.optimization.splitChunks(false).minimize(false);
+    //     webpackConfig.plugins.delete("hmr")
+    //     webpackConfig.plugins.delete("preload")
+    //     webpackConfig.plugins.delete("prefetch")
+    //     webpackConfig.plugins.delete("progress")
+    //     webpackConfig.plugins.delete("friendly-errors")
+    
+    //     console.log(webpackConfig.toConfig())
+    
+    //  }
  }
+
+ 
