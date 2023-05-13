7d17f22b74b9f3a7a6b22b5aadca8f4a3c13e456
Oladapo Daniel <oladapodaniel10@gmail.com>
2023-05-13 14:45:04 +0100

    updated commit link for socket update

--- src/views/people/PeopleList.vue
+++ src/views/people/PeopleList.vue
@@ -402,8 +402,8 @@ export default {
     const archiveLoading = ref(false)
     const applyLoading = ref(false)
     const searchingMember = ref(true)
-    const socket = io('https://whatsapp-web-server.vercel.app');
-    // const socket = io('http://localhost:3001');lllll
+    const socket = io('whatsapp-web-server-em0h37mv2-oladapodaniel.vercel.app');
+    // const socket = io('http://localhost:3001');
     const whatsappmessage = ref("")
     const whatsappRecipient = ref("")
 
