e783f704882e245e355468c92dd31624d9e980f8
Oladapo Daniel <oladapodaniel10@gmail.com>
2023-05-18 13:32:05 +0100

    displaying url

--- src/socket.js
+++ src/socket.js
@@ -9,7 +9,7 @@ export const state = reactive({
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = process.env.NODE_ENV === "production" ? undefined : "http://localhost:3001";
-console.log(process.env.NODE_ENV)
+console.log(process.env.NODE_ENV, URL)
 
 export const socket = io(URL);
 
