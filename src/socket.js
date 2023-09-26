c4be8346833beddf2956a662dca6caada97ca245
Oladapo Daniel <oladapodaniel10@gmail.com>
2023-09-26 09:55:34 +0100

    changed to koyeb server

--- src/socket.js
+++ src/socket.js
@@ -12,8 +12,9 @@ export const state = reactive({
 // const URL = process.env.NODE_ENV === "production" ? 'https://app-0816c2a3-8f36-422d-987b-4773c2e36f3b.cleverapps.io' : "http://localhost:3001";
 // const URL = process.env.NODE_ENV === "production" ? 'https://churchplus-whatsapp-server.azurewebsites.net' : "http://localhost:3001";
 // const URL = process.env.NODE_ENV === "production" ? 'https://whatsapp-server-venom.azurewebsites.net' : "http://localhost:3001";
-const URL = process.env.NODE_ENV === "production" ? 'https://churchplus-venom-server-production.up.railway.app' : "http://localhost:3001";
+// const URL = process.env.NODE_ENV === "production" ? 'https://churchplus-venom-server-production.up.railway.app' : "http://localhost:3001";
 // const URL = process.env.NODE_ENV === "production" ? 'https://whatsappserver.churchpluswebsite.com' : "http://localhost:3001";
+const URL = process.env.NODE_ENV === "production" ? 'https://churchplusserver-churchpluss.koyeb.app' : "http://localhost:3001";
 console.log(process.env.NODE_ENV, URL)
 
 export const socket = io(URL, {transports: ['websocket'], upgrade: false});
