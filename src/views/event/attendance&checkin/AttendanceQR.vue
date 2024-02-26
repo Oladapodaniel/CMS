4cac814a7d00931bc5e3b1f7acda2e7baddb1612
Otuedor Gideon <samsyotuedor40@gmail.com>
2024-02-26 11:51:30 +0100

    fixed public form issues

--- src/views/event/attendance&checkin/AttendanceQR.vue
+++ src/views/event/attendance&checkin/AttendanceQR.vue
@@ -47,6 +47,7 @@ export default {
     const loading = ref(false);
 
     const attendanceData = ref(store.getters["attendance/attendanceItemData"]);
+    console.log(attendanceData.value, 'nnnnnn')
     const user = ref(store.getters.currentUser);
     const qrCode = computed(() =>
       attendanceData.value && attendanceData.value.qrCode
